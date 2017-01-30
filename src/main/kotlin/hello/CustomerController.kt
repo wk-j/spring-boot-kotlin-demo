f45878096ebe1caa89a3c568bf19bc42ea7d9942
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-01-30 18:32:45 +0700

    Query by first name

--- src/main/kotlin/hello/CustomerController.kt
+++ src/main/kotlin/hello/CustomerController.kt
@@ -10,6 +10,10 @@ class CustomerController(val repository:CustomerRepository) {
 	@GetMapping("/")
 	fun findAll() = repository.findAll()
 
+	@GetMapping("/first/{firstName}")
+	fun findByFirstName(@PathVariable firstName: String)
+		= repository.findByFirstName(firstName)
+
 	@GetMapping("/{lastName}")
 	fun findByLastName(@PathVariable lastName:String)
 			= repository.findByLastName(lastName)
--- src/main/kotlin/hello/CustomerRepository.kt
+++ src/main/kotlin/hello/CustomerRepository.kt
@@ -3,6 +3,6 @@ package hello
 import org.springframework.data.repository.CrudRepository
 
 interface CustomerRepository : CrudRepository<Customer, Long> {
-
 	fun findByLastName(lastName: String): Iterable<Customer>
+	fun findByFirstName(firstName: String): Iterable<Customer>
 }
